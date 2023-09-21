2d25568cbdd811d0f4ddf91ed6fcee44a57b9cf0
Kiran1919 <poojarykiran19@gmail.com>
2023-09-21 11:04:14 +0530

    added project to repo

---
+++ app/about/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About Travery Media</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem,
+        atque saepe ducimus officia autem, soluta maiores ipsum minima
+        dignissimos eius vitae quaerat! Aliquam veniam beatae nulla laboriosam
+        corporis. Mollitia, iure voluptas perferendis recusandae obcaecati harum
+        officia accusamus rem ea cumque consequatur error! Vero excepturi a
+        ipsam culpa molestiae odio optio!
+      </p>
+    </div>
+  );
+}
+
+export default AboutPage;
---
+++ app/about/team/page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function TeamPage() {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+        doloribus, repellendus aut inventore ab earum iusto recusandae enim quam
+        ipsam suscipit mollitia eaque id reiciendis vero beatae. Voluptatem,
+        dolorem exercitationem.
+      </p>
+    </div>
+  );
+}
+
+export default TeamPage;
