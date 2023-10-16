a6bc8e57fc7301045e10211bcfc02a75ddc606a2
Kiran1919 <poojarykiran19@gmail.com>
2023-10-16 13:52:53 +0530

    updated project

--- app/code/repos/[name]/page.jsx
+++ app/code/repos/[name]/page.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import Repo from "@/app/components/Repo";
+import RepoDirs from "@/app/components/RepoDirs";
+import Link from "next/link";
+import React, { Suspense } from "react";
 
 function RepoPage({ params: { name } }) {
   return (
     <div className="card">
-      <h2>{name}</h2>
+      <Link href="/code/repos" className="btn btn-back">
+        Back to Repositories
+      </Link>
+      <Suspense fallback={<div>Loading repo...</div>}>
+        <Repo name={name} />
+      </Suspense>
+      <Suspense fallback={<div>Loading repo...</div>}>
+        <RepoDirs name={name} />
+      </Suspense>
     </div>
   );
 }
